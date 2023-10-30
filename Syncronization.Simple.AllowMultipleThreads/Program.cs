7b9b9d35894b43a09efc855c0b9b207764dc5c45
pkaselj <petar.kaselj.00@gmail.com>
2023-10-30 19:56:40 +0100

    Initial commit

---
+++ Syncronization.Simple.AllowMultipleThreads/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    private static readonly int NumberOfTasks = 10;
+    private static readonly int NumberOfIterations = 10;
+
+    private static readonly Random random = new Random();
+
+    // Number of threads allowed to execute some block of code at the same time
+    private const int NumberOfThreadsAllowed = 3;
+    // Semaphore - A lock that allows multiple (`NumberOfThreadsAllowed`) at the same time, but no more!s
+    private static Semaphore Semaphore = new Semaphore(NumberOfThreadsAllowed, NumberOfThreadsAllowed);
+
+    private static void DoCalculation()
+    {
+        for (int i = 0; i < NumberOfIterations; i++)
+        {
+            // Take a lock (decrement semaphore by 1)
+            Semaphore.WaitOne();
+
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} took a lock!");
+            int workDuration_ms = 1000 + random.Next(2000);
+            Thread.Sleep(workDuration_ms);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} released a lock!");
+
+            // Release the lock (increment semaphore by 1)
+            Semaphore.Release();
+        }
+    }
+    public static async Task Main()
+    {
+        List<Task> tasks = new List<Task>();
+        for (int i = 0; i < NumberOfTasks; i++)
+        {
+            var task = Task.Run(DoCalculation);
+            tasks.Add(task);
+        }
+        await Task.WhenAll(tasks);
+    }
+}
---
+++ Syncronization.Simple.Deadlock/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    private static readonly object oLock1 = new object();
+    private static readonly object oLock2 = new object();
+    private static void DoWork1()
+    {
+        lock (oLock1)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} took lock 1");
+            Thread.Sleep(1000);
+            lock (oLock2)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} took lock 2");
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} doing work...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished work!");
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} releasing lock 2");
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} released lock 1");
+        }
+    }
+
+    private static void DoWork2()
+    {
+        lock (oLock2)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} took lock 2");
+            Thread.Sleep(1000);
+            lock (oLock1)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} took lock 1");
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} doing work...");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished work!");
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} releasing lock 1");
+            }
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} released lock 2");
+        }
+    }
+
+    public static async Task Main()
+    {
+        var tasks = new Task[]
+        {
+            Task.Run(DoWork1),
+            Task.Run(DoWork2)
+        };
+
+        await Task.WhenAll(tasks);
+
+    }
+}
